b07e92a273fe547ca7c90d694db39e039e6e7af8
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2020-09-12 10:01:09 -0700

    add starting web scrapping script

---
+++ scripts/web_scrapping.py
@@ -0,0 +1,15 @@
+# Authors:
+# Date created: Sept 12 2020
+# Date modified: 
+
+# Import libraries 
+import pandas as pd
+import numpy as np
+
+def functions(par1):
+    return par1
+
+
+if __name__ == "__main__":
+    
+    print("This is the main program")
